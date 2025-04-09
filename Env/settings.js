319ebd3de5b2c6d52923131f725fe08783822351
Séjœ <jmmwang800@gmail.com>
2025-04-09 11:25:35 +0300

    Update settings.js

--- Env/settings.js
+++ Env/settings.js
@@ -1,5 +1,5 @@
  const session = process.env.SESSION || '';
-const mycode = process.env.CODE || "254";
+const mycode = process.env.CODE || "254114305147";
 const botname = process.env.BOTNAME || 'DREADED';
 const herokuAppName = process.env.HEROKU_APP_NAME || '';
 const herokuApiKey = process.env.HEROKU_API_KEY || '';
